0666f325d2863e6b8a7c77547ea538495a69ffa2
thayronFeitosa <thayronf3@gmail.com>
2021-12-16 10:39:16 +0000

    configuracao inical para criar um usuario usando o mongoose

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,14 @@
 const socket = io("http://localhost:3000");
 
-socket.on('chat_iniciado', (data) => {
-  console.log(data)
-});
+function onLoad() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const name = urlParams.get("name");
+  const email = urlParams.get("email");
+  const avatar = urlParams.get("avatar");
+
+  console.log(name, email, avatar)
+
+  socket.emit('start', { name, email, avatar })
+}
+
+onLoad();
--- src/http.ts
+++ src/http.ts
@@ -1,13 +1,17 @@
+import 'reflect-metadata';
 import express, { Request, Response } from 'express';
 import path from 'path';
 
 import { createServer } from 'http';
 import { Server } from 'socket.io';
-
+import mongoose from 'mongoose';
 
 const app = express();
 
 const server = createServer(app);
+ 
+ mongoose.connect('mongodb://localhost/rocketsocket');
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 const io = new Server(server);
